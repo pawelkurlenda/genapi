551f15ecfa1a21c64f0113eea2c39546ecb88642
kurlendapawel <kurlendapawel@gmail.com>
2024-06-30 21:15:40 +0200

    add beta generate_struct macro

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,38 @@ use crate::models::entity_definitions::EntityDefinition;
 mod macros;
 mod models;
 
+
+macro_rules! generate_struct {
+    ($entity:expr) => {
+        {
+            let entity_name = &($entity.entity);
+            println!("{}", entity_name);
+
+            for field in $entity.fields.iter() {
+                println!("{}", &field.name);
+            }
+
+            let fields = $entity.fields.iter().map(|f| {
+                let f_n = &f.name;
+                let f_t = &f.field_type;
+
+                quote::quote! {
+                    pub #f_n: #f_t,
+                }
+            });
+
+            // Correct struct implementation
+
+            quote::quote! {
+                #[derive(Debug, Serialize, Deserialize)]
+                struct #entity_name {
+                    #(#fields)*
+                }
+            }
+        }
+    };
+}
+
 fn main() {
     let paths = fs::read_dir("./generated_models").unwrap();
 
@@ -15,9 +47,12 @@ fn main() {
         entity_definitions.push(entity_definition);
     }
 
+    for entity in &entity_definitions {
+        let struct_definition = generate_struct!(entity);
+    }
+
     println!("Hello, world!");
 }
-
 
 
 // Define entity definitions here (or read from JSON as before)
